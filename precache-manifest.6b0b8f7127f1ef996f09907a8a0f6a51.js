0a2b2807cd9a62281d4f9a0fc70c6ae88eba79e1
JG <diasusb@hotmail.com>
2020-10-27 18:58:27 -0300

    Updates

---
+++ precache-manifest.6b0b8f7127f1ef996f09907a8a0f6a51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4e40e1960b14b5d8267d26061442623",
+    "url": "/crud_mern-simple/index.html"
+  },
+  {
+    "revision": "3e5d70a54f2d27105a68",
+    "url": "/crud_mern-simple/static/css/main.720cda7d.chunk.css"
+  },
+  {
+    "revision": "fb8fa1f8f251538f0ca8",
+    "url": "/crud_mern-simple/static/js/2.c8179228.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/crud_mern-simple/static/js/2.c8179228.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e5d70a54f2d27105a68",
+    "url": "/crud_mern-simple/static/js/main.184f8486.chunk.js"
+  },
+  {
+    "revision": "9af8d59e0d629de3c5cc",
+    "url": "/crud_mern-simple/static/js/runtime-main.4c8063b1.js"
+  }
+]);
