e7934829e784835db9735932a1aa96c3c3863de0
JG <diasusb@hotmail.com>
2020-10-30 21:58:21 -0300

    Nome do Autor e Home de Bem Vindo.

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import Header from './../../components/Header';
 
-export default function Home(){
+import { Container, Grid, Paper } from '@material-ui/core';
 
-  return(
+export default function Home() {
+
+  return (
     <>
       <Header />
+      <Container maxWidth="lg" className={classes.containerMain} >
+        <Grid container spacing={2} justify="center" alignItems="center" style={{ marginTop: 50 }}>
+          <Grid item xs={12} style={{ textAlign: "center" }}>
+            <Paper elevation={3}>
+              <h1 style={{ color: '#6f42c1' }}>
+                Olá, Bem vindo Visitante.
+              </h1>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
     </>
   );
 }
